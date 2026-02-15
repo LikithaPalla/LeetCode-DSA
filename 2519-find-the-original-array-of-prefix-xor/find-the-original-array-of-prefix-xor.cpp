0d604e7cfb2f83114e373f143b1903af6feb1eb9
Likitha Palla <157525655+LikithaPalla@users.noreply.github.com>
2026-02-15 19:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 80.5 MB (71.64%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> findArray(vector<int>& pref) {
         vector<int> original(pref.size());
         original[0]=pref[0];
-        int xor_op = original[0];
         for(int i=1;i<pref.size();i++)
         {
-            original[i]=xor_op ^ pref[i];
-            xor_op = xor_op ^ original[i];
+            original[i]= pref[i] ^ pref[i-1];
         }
         return original;
     }
